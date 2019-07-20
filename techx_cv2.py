9852aa6bfa606b74cd4b27bec43c18cdb019169d
ScarletRain <992127134@qq.com>
2019-07-20 15:56:06 +0800

    1

---
+++ techx_cv2.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from PIL import Image
+img=Image.new('RGB',(128,128),'white')
+img.save(r'C:\Users\tangj\Desktop\diagonal.png')
+
+im_edit=cv2.imread(r'C:\Users\tangj\Desktop\diagonal.png')
+b,g,r=cv2.split(im_edit)
+for i in range(b.shape[0]):
+
+    b[i,i]=0
+    g[i,i]=0
+    r[i,i]=0
+
+im2_edit=cv2.merge((r,g,b)
+cv2.imwrite(r'C:\Users\tangj\Desktop\p.png',im2_edit)
