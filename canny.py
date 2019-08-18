988a085762dce4076ee8052b78c5348853f98d2e
ScarletRain <992127134@qq.com>
2019-08-18 14:48:37 +0800

    1

--- canny.py
+++ canny.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
-im=Image.open(r'C:\Users\tangj\Desktop\yeah.jpg').convert('L')
+im=Image.open(r'C:\Users\tangj\Desktop\P1.jpg').convert('L')
 img=np.array(im).astype(np.float32)/255
 img_edge=cv2.Canny(np.uint8(255*img),30,155)
 plt.imshow(img_edge)
