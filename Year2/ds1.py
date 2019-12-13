a1187b6d75c08eb91192145a371ae6bdd82d5bda
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2019-12-13 19:52:23 +0000

    1

--- Year2/ds1.py
+++ Year2/ds1.py
@@ -27,6 +27,35 @@ class Stack():
     def size(self):
         return len(self.items)
 
+class Node:
+    def __init__(self,initdata):
+        self.data=initdata
+        self.next=None
+    def getData(self):
+        return self.data
+    def getNext(self):
+        return self.next
+    def setData(self,newdata):
+        self.data=newdata
+    def setNext(self,newnext):
+        self.next=newnext
+
+class Linked_list(object):
+    def __init__(self):
+        self.head=None
+    def add(self,item):
+        temp=Node(item)
+        temp.setNext(self.head)
+        self.head=temp
+    def size(self):
+        current=self.head
+        count=0
+        while current != None:
+            count = count+1
+            current = current.getNext()
+        return count
+    
+
 
 
 if __name__ == "__main__":
--- Year2/ds_p.py
+++ Year2/ds_p.py
@@ -1,14 +1,16 @@
 from ds1 import Stack, Queue
 
+
 def hotPotato(namelist, num):
     simqueue=Queue()
     for name in namelist:
         simqueue.enqueue(name)
     while simqueue.size()>1:
         for i in range(num):
-            simqueue.enqueue(simqueue.dequeue())
-        simqueue.dequeue()
+            simqueue.enqueue(simqueue.dequeue())# transfer the potato
+        simqueue.dequeue()#delete the one who lose
     return simqueue.dequeue()
+
 
 
 if __name__ == "__main__":
