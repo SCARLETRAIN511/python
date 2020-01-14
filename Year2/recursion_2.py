7ea05e4b84f1a4d057727044dc82e9a04c5d95b8
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-01-14 19:19:00 +0000

    1

---
+++ Year2/recursion_2.py
@@ -0,0 +1,30 @@
+def recMc(coinValueList, change):
+    minCoins = change
+    if change in coinValueList:
+        return 1
+    else:
+        for i in [c for c in coinValueList if c <= change]:
+            numCoins = 1 + recMc(coinValueList, change - i)
+            if numCoins < minCoins:
+                minCoins = numCoins
+    return minCoins
+
+def recDc(coinValueList, change, knownResults):
+    minCoins = change
+    if change in coinValueList:
+        knownResults[change] = 1
+        return 1
+    elif knownResults[change] > 0:
+        return knownResults[change]
+    else:
+        for i in [c for c in coinValueList if c <= change]:
+            numCoins = 1 + recDc(coinValueList, change - i, knownResults)
+            if numCoins < minCoins:
+                minCoins = numCoins
+                knownResults[change] = minCoins
+    return minCoins
+
+
+if __name__ == "__main__":
+    print(recDc([1,5,10,50,100], 70, [0]*200))
+
