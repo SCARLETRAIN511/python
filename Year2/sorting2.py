c3e90a20090a17992c3fe1ed2ffe87921ea53059
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-01-16 19:20:36 +0000

    1

--- Year2/sorting2.py
+++ Year2/sorting2.py
@@ -39,6 +39,20 @@ def selectionSorting(alist):
                 positionOfMax = location #get the location of the maximum number in the list
         alist[fillshot], alist[positionOfMax] = alist[positionOfMax], alist[fillshot]
     return alist
+
+#insertion sorting
+def insertionSorting(alist):
+    for index in range(1, len(alist)):
+        currentvalue = alist[index]
+        position = index
+        while position > 0 and alist[position - 1] > currentvalue:#do the comparsion
+            alist[position] = alist[position - 1]#move the value in the list 1 unit next
+            position -= 1
+        alist[position] = currentvalue
+    return alist
+
+
 if __name__ == "__main__":
     print(bubbleSorting1([1,2424,2,13,3154,13]))
     print(selectionSorting([15,14,13,12,12,5,6,3,2]))
+    print(insertionSorting([5,3,4,1,244,12]))
