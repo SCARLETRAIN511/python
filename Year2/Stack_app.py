0d9ed9c30fc50addc1d90a9180dcb3d6bbfbe420
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-01-21 20:14:00 +0000

    1

--- Year2/Stack_app.py
+++ Year2/Stack_app.py
@@ -1,6 +1,7 @@
 from ds1 import Deque
 from ds1 import Stack
 
+
 def parChecker(symbolString):
     s = Stack()
     balanced = True
@@ -58,6 +59,40 @@ def baseConverter(decNumber, base):
     return newStr
 
 
+#中缀转后缀算法
+def infixToPosfix(infixexpr):
+    prec = {}
+    prec['*'] = 3
+    prec['/'] = 3
+    prec['+'] = 2
+    prec['-'] = 2
+    prec['('] = 1
+    opStack = Stack()
+    postfixList = []
+    tokenList = infixexpr.split()
+
+    for token in tokenList:
+        if token in "ABCDEFGHIZKLMNOPQRSTUVWXYZ" or token in "0123456789":
+            postfixList.append(token)
+        elif token == '(':
+            opStack.push(token)
+        elif token == ')':
+            topToken = opStack.pop()
+            while topToken != '(':
+                postfixList.append(topToken)
+                topToken = opStack.pop()
+        else:
+            while (not opStack.isEmpty()) and (prec[opStack.peek()] >= prec[token]):
+                postfixList.append(opStack.pop())
+            opStack.push(token)
+
+    while not opStack.isEmpty():
+        postfixList.append(opStack.pop())
+
+    return " ".join(postfixList)
+
+
+
 def palchecker(astring):
     chardeque = Deque()
 
@@ -79,3 +114,4 @@ if __name__ == "__main__":
     print(palchecker("lljshishabi"))
     print(binary(1213))
     print(baseConverter(123,16))
+    print(infixToPosfix('( A + B ) * C'))
