c37aaa262dc3e87f3bd1dae14cc3843672729d28
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-01-22 18:27:09 +0000

    1

--- Year2/ds_printer.py
+++ Year2/ds_printer.py
@@ -1,18 +1,21 @@
 from PythonDSExamplePrograms.pythonds.basic.queue import Queue
 import random
 
+
+#using s as the unit of time here
 class Printer:
     def __init__(self,ppm):
-        self.pagerate = ppm
+        self.pagerate = ppm#speed of printing
         self.currentTask = None
-        self.timeRemaining = 0
+        self.timeRemaining = 0#timer for the task
 
-    def tick(self):
+    def tick(self):#print for 1 sec here
         if self.currentTask != None:
             self.timeRemaining = self.timeRemaining - 1
             if self.timeRemaining <= 0:
                 self.currentTask = None
     
+    #if busy or not of the printer
     def busy(self):
         if self.currentTask != None:
             return True
@@ -21,13 +24,13 @@ class Printer:
 
     def startNext(self, newtask):
         self.currentTask = newtask
-        self.timeRemaining = newtask.getPages()*60/(self.pagerate)
+        self.timeRemaining = newtask.getPages()*60/(self.pagerate)#get the time remaining for the task
 
 
 class Task:
     def __init__(self,time):
-        self.timestamp = time
-        self.pages = random.randrange(1,21)
+        self.timestamp = time#generate the timestamp
+        self.pages = random.randrange(1,21)#randomly generate the number of pages
     
     def getStamp(self):
         return self.timestamp
@@ -38,6 +41,7 @@ class Task:
     def waitTime(self, currenttime):
         return currenttime - self.timestamp
 
+#randomly generate the task for printing
 def newPrintTask():
     num = random.randrange(1,181)
     if num == 180:
@@ -45,10 +49,11 @@ def newPrintTask():
     else:
         return False
 
+#start the simulation. PagesPerMinute set the rate of the printer
 def simulation(numSeconds, pagesPerMinute):
 
-    labprinter = Printer(pagesPerMinute)
-    printQueue = Queue()
+    labprinter = Printer(pagesPerMinute)#initate the printer
+    printQueue = Queue()#get the buffer area
     waitingtimes = []
     
     for currentSecond in range(numSeconds):
