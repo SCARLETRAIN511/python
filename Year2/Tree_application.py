410d9bd8b70111a93c631c459167074970c7474f
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-01-29 21:55:48 +0000

    1

--- Year2/Tree_application.py
+++ Year2/Tree_application.py
@@ -126,9 +126,123 @@ class BinaryHeap:
         print(self.heapList,i)
 
 
+#二叉查找树
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
 
+    def length(self):
+        return self.size
 
+    def __len__(self):
+        return self.size 
+
+    def __iter__(self):
+        return self.root.__iter__()
     
+    def put(self,key,val):
+        if self.root:
+            self.__put(key,val,self.root)
+        else:
+            self.root = TreeNode(key,val)
+        self.size += 1
+
+    def __put(self,key,val, currentNode):
+        if key < currentNode.key:
+            if currentNode.hasLeftChild():
+                self.__put(key,val, currentNode.leftChild)
+            else:
+                currentNode.leftChild = TreeNode(key,val,parent=currentNode)
+                if currentNode.hasRightChild():
+                    self.__put(key,val,currentNode.rightChild)
+                else:
+                    currentNode.rightChild = TreeNode(key,val,parent=currentNode)
+    
+    def __setitem(self,k,v):
+        self.put(k,v)
+
+    def get(self,key):
+        if self.root:
+            res = self.__get(key,self.root)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+    
+    def __get(self,key,currentNode):
+        if not currentNode:
+            return None
+        elif currentNode.key == key:
+            return currentNode
+        elif key < currentNode.key:
+            return self.__get(key, currentNode.leftChild)
+        else:
+            return self.__get(key, currentNode.rightChild)
+    
+    def __getitem__(self,key):
+        return self.get(key)
+    
+    def __contains__(self,key):
+        if self.__get(key,self.root):
+            return True
+        else:
+            return False
+
+
+class TreeNode:
+    def __init__(self,key,val,left = None,right = None, parent = None):
+        self.key = key
+        self.payload = val
+        self.leftChild = left
+        self.rightChild = right
+        self.parent = parent
+
+    def hasLeftChild(self):
+        return self.leftChild
+    
+    def hasRightChild(self):
+        return self.rightChild
+    
+    def isLeftChild(self):
+        return self.parent and self.parent.leftChild == self
+
+    def isRightChild(self):
+        return self.parent and self.parent.rightChild == self
+
+    def isRoot(self):
+        return not self.parent
+    
+    def isleaf(self):
+        return not (self.rightChild or self.leftChild)
+
+    def hasAnyChildren(self):
+        return self.rightChild or self.leftChild
+    
+    def hasBothChild(self):
+        return self.rightChild and self.leftChild
+    
+    def replaceNodeData(self,key,value,lc,rc):
+        self.key = key
+        self.payload = value
+        self.leftChild = lc
+        self.rightChild = rc
+        if self.hasLeftChild():
+            self.leftChild.parent = self
+        if self.hasRightChild():
+            self.rightChild.parent = self
+
+    def __iter__(self):
+        if self:
+            if self.hasLeftChild():
+                for elem in self.leftChild:
+                    yield elem
+            yield self.key
+            if self.hasRightChild():
+                for elem in self.rightChild:
+                    yield elem
 
 
 
