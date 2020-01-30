b85bd63d611e4f3c412da5d0fa564c8b801d85f5
ScarletRain <992127134@qq.com>
2020-01-30 03:33:49 +0000

    Update recog3.py

--- Year2/recog3.py
+++ Year2/recog3.py
@@ -2,7 +2,7 @@ import pytesseract
 from PIL import Image
 
 def main():
-    filename = input('Input the name of the image inside this file')
+    filename = input('Input the name of the image inside this file: ')
     pytesseract.pytesseract.tesseract_cmd = 'C://Program Files (x86)/Tesseract-OCR/tesseract.exe'
     text = pytesseract.image_to_string(Image.open(filename))
     print(text)
