da2f2ded75c6f0742632a36835710ba6ebdb200f
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-18 19:01:57 +0000

    1

---
+++ DataStructure1/gcdSelf.py
@@ -0,0 +1,18 @@
+def solution(a,b):
+    best = 0
+    for i in range(1,min(a,b)+1):
+        if a%i==0 & b%i==0:
+            if i>best:
+                best = i
+    return best
+
+def solutionEfficient(a,b):
+    if b == 0:
+        return a
+    else:
+        return(solutionEfficient(b,a%b))
+
+
+if __name__ == "__main__":
+    print(solution(4,8))
+    print(solutionEfficient(357,234))
