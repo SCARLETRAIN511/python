358dc7ea2031767fe0c353146fa5b78c5642a425
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-02-27 20:56:02 +0000

    Update class.py

--- ds/DataStructure1_4_divide/class.py
+++ ds/DataStructure1_4_divide/class.py
@@ -19,5 +19,35 @@ def linearSearchRecursion(A,low,high,key):
         return low
     return linearSearchRecursion(A,low+1,high,key)
 
+
+#binary search
+##searching sorted data
+
+def binarySearch(A,low,high,key):
+    A.sort()
+    if high<low:
+        return low - 1
+    mid = int(low + (high-low)/2)
+    if key == A[mid]:
+        return mid
+    elif key < A[mid]:
+        return binarySearch(A,low,mid-1,key)
+    else:
+        return binarySearch(A,mid+1,high,key)
+
+##polynomial multiplication
+#naive algorithm
+
+def MultPoly(A,B,n):
+    product = [0 for i in range(2*n-1)]
+    for i in range(n):
+        for j in range(n):
+            product[i+j]=product[i+j]+A[i]*B[j]
+    return product
+
+
+
 if __name__ == "__main__":
-    print(linearSearchRecursion([1,2,3,45,6],0,4,1))
+    print(MultPoly([1,2],[3,4],2))
+        
+
