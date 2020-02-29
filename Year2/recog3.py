acda50db063c1479a08e5fe709db33cbfa77028d
ScarletRain <992127134@qq.com>
2020-02-29 12:51:12 +0000

    Update recog3.py

--- Year2/recog3.py
+++ Year2/recog3.py
@@ -1,11 +1,21 @@
 import pytesseract
 from PIL import Image
+import cv2
 
 def main():
-    filename = input('Input the name of the image inside this file: ')
-    pytesseract.pytesseract.tesseract_cmd = 'C://Program Files (x86)/Tesseract-OCR/tesseract.exe'
-    text = pytesseract.image_to_string(Image.open(filename))
-    print(text)
+    cap = cv2.VideoCapture(0)
+    print("摄像头是否已经打开 ？ {}".format(cap.isOpened()))
+
+    while cap.isOpened():
+        ret,frame = cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        pytesseract.pytesseract.tesseract_cmd = 'C://Program Files (x86)/Tesseract-OCR/tesseract.exe'
+        text = pytesseract.image_to_string(gray)
+        cv2.imshow('frame',gray)
+        print(text)
+
+    cap.release()
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
