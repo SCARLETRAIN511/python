350c46d25dc38b273bf37d1ee51ba59e1b3b179e
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-03-03 22:51:39 +0000

    1

--- ds/DataStructure1_5_DynamicProgramming1/class.py
+++ ds/DataStructure1_5_DynamicProgramming1/class.py
@@ -13,6 +13,19 @@ def RecursiveNumCoins(money,coins):
                 MinNumCoins = NumCoins + 1
     return MinNumCoins
 
+## dynamic programming method 
+
+def DPchange(money,coins):
+    MinNumCoins = [0 for i in range(0,money+1)]
+    for m in range(1,money+1):
+        MinNumCoins[m] = 100000
+        for coin in coins:
+            if m >= coin:
+                NumCoins = MinNumCoins[m-coin] + 1
+                if NumCoins < MinNumCoins[m]:
+                    MinNumCoins[m] = NumCoins
+    return MinNumCoins[money]
 
 if __name__ == "__main__":
     print(RecursiveNumCoins(2,[6,5,1]))
+    print(DPchange(997,[2,4,8]))
