730b956269ac8e10626d57f6d467104cbb1ec456
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-03-07 18:28:08 +0000

    1

--- ds/DataStructure1_5_DynamicProgramming1/primitive_calculator.py
+++ ds/DataStructure1_5_DynamicProgramming1/primitive_calculator.py
@@ -2,16 +2,48 @@
 import sys
 
 def optimal_sequence(n):
-    sequence = []
-    while n >= 1:
-        sequence.append(n)
-        if n % 3 == 0:
-            n = n // 3
-        elif n % 2 == 0:
-            n = n // 2
+    min_num_operations = [n] * n
+    min_num_operations[0] = 0
+
+    ops = [-1]*n
+    op_types = (1,2,3)
+    for i in range(n-1):
+        for op_type in op_types:
+            n_i = i + 1
+            if op_type == 1:
+                n_new = n_i * 2
+            elif op_type == 2:
+                n_new = n_i *3
+            elif op_type == 3:
+                n_new = n_i + 1
+            else:
+                raise ValueError('New operation type {}'.format(op_type))
+            num_operations = min_num_operations[i] + 1
+            i_new = n_new - 1
+            
+            if n_new <= n and num_operations<min_num_operations[i_new]:
+                min_num_operations[i_new] = num_operations
+                ops[i_new] = op_type
+    seq = getSequence(n,ops)
+    return seq
+
+def getSequence(n,ops):
+    n_seq = n
+    seq = [n]
+    while n_seq != 1:
+        i = n_seq - 1
+        op_type = ops[i]
+        if op_type == 1:
+            n_seq = n_seq//2
+        elif op_type == 2:
+            n_seq = n_seq//3
+        elif op_type == 3:
+            n_seq = n_seq - 1
         else:
-            n = n - 1
-    return reversed(sequence)
+            raise ValueError("New operation type{}".format(op_type))
+        seq.append(n_seq)
+    return seq[::-1]
+
 
 input = sys.stdin.read()
 n = int(input)
