3d99c9d7f375a4ddf47e3d2b81e322f1e19a0bf9
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-06-12 19:10:12 +0800

    Update dynamicArray.py

--- DataStructureCousera/DS2_2_DynamicArrays/dynamicArray.py
+++ DataStructureCousera/DS2_2_DynamicArrays/dynamicArray.py
@@ -1,6 +1,8 @@
 #dynamic array
 #abstract data types
+#python's list is the dynamic array
 
+#bit of coding here
 class DynamicArray():
     def __init__(self):
         self.data = []
@@ -22,4 +24,14 @@ class DynamicArray():
     def remove(self,i):
         del self.data[i]
 
+
+#amotized cost aggregate method = cost(n operations)/n
+
+#amortized method banker's method
+#charge extra for each cheap operation. Save the extra charge as tokens in tour ds, use the
+#tokens to pay for expensive operations 
+
+#for physict's method
+#Define a potential function psi which map states of the data structure to integers
+#amotized cost for the operation T= ct+psi(ht) - psi(ht-1)
 
