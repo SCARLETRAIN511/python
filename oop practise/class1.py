750ee8973cefa4d467bf31b3cfc7d20583c4a906
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-06-14 16:29:41 +0800

    Update class1.py

--- oop practise/class1.py
+++ oop practise/class1.py
@@ -1,10 +1,22 @@
+#class variables are defined ouside the initializer and instance variables are defined inside the initializer
+
 class Employee():
-    def __init__(self,id,age,salary,department):
+    #class variables
+    Employeelist = []
+
+    def __init__(self,id,age,salary=13000,department="mechanical"):
+        #instance variables
         self.id = id
         self.age = age
         self.salary = salary
         self.department = department
+        self.Employeelist.append(self.id)
+
 
 Steve = Employee(1,19,19000,'electrics')
 
-print(Steve.id)
+Mark = Employee(2,20,190000,'mechanical')
+
+
+print(Mark.Employeelist)
+print(Employee.Employeelist)
