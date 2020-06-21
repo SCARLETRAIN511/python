2eae6b0f2374fb3a9cce93ebfa8d235b20bb012a
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-06-21 16:23:03 +0800

    Update linkedList.py

--- oop practise/linkedList.py
+++ oop practise/linkedList.py
@@ -241,8 +241,26 @@ class LinkedList:
         else:
             return self.countOccurenceRecur(node.next,data)
 
-
-
+    def rotate(self,k):
+        if self.head and self.head.next:
+            p = self.head
+            q = self.head
+            prev = None
+            count = 0
+            while p and count < k:
+                prev = p
+                p = q.next
+                q = q.next
+                count += 1
+            p = prev
+            while q:
+                prev = q
+                q = q.next
+            q = prev
+            q.next = self.head
+            self.head = p.next
+            p.next = None
+            
     
 def main():
     llist = LinkedList()
