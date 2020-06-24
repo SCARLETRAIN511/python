a684227b567f1e4913bbe2dff66d996dd5cefb2f
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-06-24 11:53:54 +0800

    Update CircularLinkedList.py

--- oop practise/CircularLinkedList.py
+++ oop practise/CircularLinkedList.py
@@ -7,5 +7,36 @@ class CircularLinkedList:
     def __init__(self):
         self.head = None
     
-    def prepend(self,data):
-        pass
+    def append(self,data):
+        if not self.head:
+            self.head = Node(data)
+            #this is a circular linkedList
+            self.head.next = self.head
+        else:
+            newNode = Node(data)
+            curr = self.head
+            while curr.next != self.head:
+                curr = curr.next
+            curr.next = newNode
+            newNode.next = self.head
+    
+    def printList(self):
+        curr = self.head
+        listValue = []
+        while curr:
+            listValue.append(curr.data)
+            curr = curr.next
+            if curr == self.head:
+                break
+        print(listValue)
+
+
+def op1():
+    llist = CircularLinkedList()
+    llist.append(1)
+    llist.append(2)
+
+    llist.printList()
+
+if __name__ == "__main__":
+    op1()
