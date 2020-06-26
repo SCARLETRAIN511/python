ec8038be0c50a8c8d4f42316165ae72431045e75
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-06-26 21:48:46 +0800

    Update doubleLinkedList.py

--- oop practise/doubleLinkedList.py
+++ oop practise/doubleLinkedList.py
@@ -25,3 +25,36 @@ class DoubleLinkedList:
             newNode.prev = curr
             newNode.next = None
     
+    def printList(self):
+        curr = self.head
+        listNode = []
+        while curr:
+            listNode.append(curr.data)
+            curr = curr.next
+        
+        print(listNode)
+
+    def prepend(self,data):
+        if self.head is None:
+            newNode = Node(data)
+            newNode.prev = None
+            self.head = newNode
+        else:
+            newNode = Node(data)
+            self.head.prev = newNode
+            newNode.next = self.head
+            self.head = newNode
+            newNode.prev = None
+
+
+def op1():
+    llist = DoubleLinkedList()
+    llist.append(1)
+    llist.append(2)
+    llist.append(4)
+    llist.prepend(5)
+    llist.printList()
+
+
+if __name__ == "__main__":
+    op1()
