71d15fdb40bc197bfa8a9ae1f29cff5e024c9af1
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-07-05 21:33:35 +0800

    Update BinarySearchTree.py

--- oop practise/BinarySearchTree.py
+++ oop practise/BinarySearchTree.py
@@ -3,8 +3,8 @@
 class Node:
     def __init__(self,data):
         self.data = data
-        self.left = left
-        self.right = right
+        self.left = None
+        self.right = None
 
 class BST:
     def __init__(self,root):
@@ -24,3 +24,28 @@ class BST:
                 self.insertHelper(current.left,newVal)
             else:
                 current.left = Node(newVal)
+    
+    def search(self, val):
+        return self.searchHelper(self.root,val)
+    
+    def searchHelper(self,current,findVal):
+        if current:
+            if current.data == findVal:
+                return True
+            elif current.data < findVal:
+                return self.searchHelper(current.right,findVal)
+            else:
+                return self.searchHelper(current.left,findVal)
+
+
+def bstOp():
+    bst1 = BST(10)
+    bst1.insert(2)
+    bst1.insert(14)
+    bst1.insert(4)
+
+    print(bst1.search(4))
+
+
+if __name__ == "__main__":
+    bstOp()
