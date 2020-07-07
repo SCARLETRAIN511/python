ebf153fddfb6dc2fa967c4d1a0e9004dadeb116e
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-07-07 16:28:54 +0800

    Update Bisection Algorithms.py

--- oop practise/Bisection Algorithms.py
+++ oop practise/Bisection Algorithms.py
@@ -77,6 +77,18 @@ def intergerSquareRoot(k):
     return low - 1
 
 
+    #Cyclically shifted array
+def integerSquareRoot(A):
+    low = 0
+    high = len(A) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if A[mid] > A[high]:
+            low = mid + 1
+        elif A[mid] <= A[high]:
+            high = mid
+    return low 
+
 
 if __name__ == "__main__":
     print(findFixedPoint([1,2,3,4,5,55,343,894]))
