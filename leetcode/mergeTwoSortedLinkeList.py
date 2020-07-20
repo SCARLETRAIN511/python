f981629daff871110cf220db055929762148cea8
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-07-20 15:40:13 +0800

    Create mergeTwoSortedLinkeList.py

---
+++ leetcode/mergeTwoSortedLinkeList.py
@@ -0,0 +1,27 @@
+#python3
+
+#merge 2 sorted linkedlist
+class ListNode:
+    def __init__(self,val = 0,next = None):
+        self.val = val
+        self.next = None
+
+
+class Solution:
+    def mergeTwoLinkedList(self,l1,l2):
+        prehead = ListNode(-1)
+        prev = prehead
+        while l1 and l2:
+            if l1.val < l2.val:
+                prev.next = l1
+                l1 = l1.next
+            else:
+                prev.next = l2
+                l2 = l2.next
+            prev = prev.next
+
+        if l1 is not None:
+            prev.next = l1
+        else:
+            prev.next = l2
+        return prehead.next
