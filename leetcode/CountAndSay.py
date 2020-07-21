0620d69160814b63bb02c76fe2cc8a4c248ca713
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-07-21 15:39:04 +0800

    update

--- leetcode/CountAndSay.py
+++ leetcode/CountAndSay.py
@@ -9,10 +9,12 @@ class Solution:
         for i in range(1,n):
             nextPerson,num,count = "",prevPerson[0],1
             for j in range(1,len(prevPerson)):
+                #如果出现的数一致，数量 + 1
                 if prevPerson[j] == num:
                     count += 1
+                #如果不一致，先添加已有的描述，更换数字与次数
                 else:
-                    nextPerson += str(count) + num
+                    nextPerson += (str(count) + num)
                     num = prevPerson[j]
                     count = 1
             nextPerson += str(count) + num
