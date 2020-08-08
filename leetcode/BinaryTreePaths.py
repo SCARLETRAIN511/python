40cb3e0cd5655e8f9b8cd2d0727859989dd54f61
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-08 23:50:23 +0800

    Update BinaryTreePaths.py

--- leetcode/BinaryTreePaths.py
+++ leetcode/BinaryTreePaths.py
@@ -22,5 +22,31 @@ class Solution:
         paths = []
         construcPaths(root,"")
         return paths
+    
+    
+    def binaryTreePaths2(self,root):
+        if not root:
+            return []
         
+        paths = []
+        stack = [(root,str(root.val))]
+        while stack:
+            node,path = stack.pop()
+            if not node.left and not node.right:
+                paths.append(path)
+            if node.left:
+                stack.append((node.left, path + "->" + str(node.left.val)))
+            if node.right:
+                stack.append((node.right, path + "->" + str(node.right.val)))
+        return paths
+
+
+if __name__ == "__main__":
+    s = Solution()
+    t1 = TreeNode(1)
+    t2 = TreeNode(2)
+    t3 = TreeNode(3)
+    t1.right = t2
+    t1.left = t3
+    print(s.binaryTreePaths2(t1))
 
