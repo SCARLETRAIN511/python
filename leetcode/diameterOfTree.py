36bd4bfe2f992c94ce7dfafb38f82e85af911cb5
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-12 09:14:04 +0800

    Create diameterOfTree.py

---
+++ leetcode/diameterOfTree.py
@@ -0,0 +1,27 @@
+#python3
+
+#calculate the diameter of the binary tree
+#the diameter of the binary tree os is the maximum length between any 2 points.
+
+
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.right = None
+        self.left = None
+
+
+class Solution:
+    def diameterOfBinaryTree(self,root) -> int:
+        self.ans = 1
+        def depth(root):
+            if not root:
+                return 0
+
+            left = depth(node.left)
+            right = depth(node.right)
+
+            self.ans = max(self.ans,left + right + 1)
+            return max(left,right) + 1
+        depth(root)
+        return self.ans - 1
