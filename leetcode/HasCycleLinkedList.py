c18da9bc60e3666b2eab3049e368198872b73a27
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-15 20:22:44 +0800

    Update HasCycleLinkedList.py

--- leetcode/HasCycleLinkedList.py
+++ leetcode/HasCycleLinkedList.py
@@ -18,3 +18,19 @@ class Solution:
             head = head.next
         return False
         
+    def hasCycle2(self,head) -> bool:
+        if head == None or head.next == None:
+            return False
+        slow = head
+        fast = head.next
+        while slow != fast:
+            if fast == None or fast.next == None:
+                return False
+            slow = slow.next
+            fast = fast.next.next
+        
+        return True
+
+        
+
+
