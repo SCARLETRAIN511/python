a46dff118d4cba933c5cb9a0cd78c3d47fcff8b8
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-19 10:47:03 +0800

    Update RotateArray.py

--- leetcode/RotateArray.py
+++ leetcode/RotateArray.py
@@ -6,8 +6,19 @@ class Solution:
         for i in range(k):
             nums.insert(0,nums.pop())
         print(nums)
+    
+    def rotate2(self,nums,k) -> None:
+        a = [0]*len(nums)
+        print(a)
+
+        for i in range(len(nums)):
+            a[(i + k) % len(nums)] = nums[i]
+        
+        for i in range(len(nums)):
+            nums[i] = a[i]
+        print(nums)
 
 
 if __name__ == "__main__":
     s = Solution()
-    s.rotate([1,2,3,4,5,],3)
+    s.rotate2([1,2,3,4,5],3)
