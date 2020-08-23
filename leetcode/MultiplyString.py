ee76edf21d8fc31e4d344faa253e8ef0c61eba4e
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-08-23 10:20:32 +0800

    Update MultiplyString.py

--- leetcode/MultiplyString.py
+++ leetcode/MultiplyString.py
@@ -1,5 +1,7 @@
 #python3
 
+
+#solution1 模拟竖乘
 class Solution:
     def multiPly(self,num1:str,num2:str) -> str:
         if num1 == "0" or num2 == "0":
@@ -37,6 +39,30 @@ class Solution:
         return "".join(ans[::-1])
 
 
+#做乘法
+class Solution2:
+    def multiply(self,num1:str,num2:str) -> str:
+        if num1 == "0" or num2 == "0":
+            return "0"
+        
+        m,n = len(num1),len(num2)
+        ansArr = [0] * (m + n)
+        for i in range(m-1,-1,-1):
+            x = int(num1[i])
+            for j in range(n-1,-1,-1):
+                ansArr[i + j + 1] += x * int(num2[j])
+
+        for i in range(m + n - 1,0,-1):
+            ansArr[i-1] += ansArr[i] // 10
+            ansArr[i] %= 10
+        
+        index = 1if ansArr[0] == 0 else 0
+        ans = "".join(str(x) for x in ansArr[index:])
+        return ans
+
+
 if __name__ == "__main__":
     s = Solution()
     print(s.multiPly("5","6"))
+    s2 = Solution2()
+    print(s2.multiply("5","4"))
