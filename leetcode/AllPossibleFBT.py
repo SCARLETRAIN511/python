1d034551229809a92edd8d3e1b0d2977f2b5ee88
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-09-02 12:02:16 +0800

    Create AllPossibleFBT.py

---
+++ leetcode/AllPossibleFBT.py
@@ -0,0 +1,33 @@
+#python3
+
+class TreeNode:
+    def __init__(self,x):
+        self.val = x
+        self.left = None
+        self.right = None
+    
+#for full binary tree, every node has no or 2 childs
+
+class Solution:
+    memo = {0:[],1:[TreeNode(0)]}
+
+    def allPossibleFBT(self,N:int):
+        #return the list with treeNodes.
+        if N not in Solution.memo:
+            ans = []
+            for x in range(N):
+                y = N - 1 -x
+                for left in self.allPossibleFBT(x):
+                    for right in self.allPossibleFBT(y):
+                        bns = TreeNode(0)
+                        bns.left = left
+                        bns.right = right
+                        ans.append(ans)
+            Solution.memo[N] = ans
+        
+        return Solution.memo[N]
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.allPossibleFBT(5))
