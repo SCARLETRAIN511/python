a31a2f5c68040a615d6be94eecf50f1bbd3a0ba2
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-09-04 10:12:14 +0800

    Update fib.py

--- leetcode/fib.py
+++ leetcode/fib.py
@@ -3,7 +3,7 @@ class Solution:
     def fib(self,n):
         if n == 0:
             return 0
-        dp = []
+        dp = [0]*(n+1)
         dp[0] = 0
         dp[1] = 1
         for i in range(2,n+1):
@@ -12,8 +12,12 @@ class Solution:
         
         return dp[n]
 
-    def fb2(self,n):
+    def fib2(self,n):
         a,b = 0,1
         for i in range(n):
             a,b = b, a+b
         return a % 1000000007
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.fib2(5))
