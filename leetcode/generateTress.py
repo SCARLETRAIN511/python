2a3c4df91c3c9b8e2bac8148f78bb6d90bceb6f2
Jiaxuan Tang <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-09-10 09:22:26 +0800

    Update generateTress.py

--- leetcode/generateTress.py
+++ leetcode/generateTress.py
@@ -28,3 +28,29 @@ class Solution:
         
         return generateTrees(1,n)if n else []
 
+
+#same as the solution1, but only return the number
+
+class Solution2:
+    def numTrees(self,n) -> int:
+        c = 1
+        for i in range(0,n):
+            c = c*2*(2*i+1)/(i+2)
+        return int(c)
+
+    def numTrees2(self,n) -> int:
+        G = [0]*(n+1)
+        G[0],G[1] = 1,1
+        for i in range(2,n+1):
+            for j in range(1,i+1):
+                G[i] += G[j-1] * G[i-j]
+        
+        return G[n]
+
+
+if __name__ == "__main__":
+    s = Solution2()
+    print(s.numTrees2(5))
+        
+
+
