9fa6a0834a614934cee815dc62028f185a0eef9e
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-10-08 22:06:05 +0800

    Create mystack..py

---
+++ leetcode/mystack..py
@@ -0,0 +1,21 @@
+#python3
+#use queue to realize stack
+import collections
+
+class MyStack:
+    def __init__(self):
+        self.queue = collections.deque()
+    def push(self,x) -> None:
+        n = len(self.queue)
+        self.queue.append(x)
+        for i in range(n):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+    
+    def empty(self) -> bool:
+        return not self.queue
