ef9b6eb93abbf4fa9c9c96fde59bacbeb56fde56
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-10-27 19:05:36 +0800

    Create getKthFromEnd.py

---
+++ leetcode/getKthFromEnd.py
@@ -0,0 +1,22 @@
+#python3
+
+class ListNode:
+    def __init__(self,x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def getKthFromEnd(self,head,k):
+        #return the listnode
+        #use 2 pointers
+        former,latter = head,head
+        for _ in range(k):
+            if not former:
+                return
+                former = former.next
+        
+        while former:
+            former,latter = former.next,latter.next
+
+        return latter
