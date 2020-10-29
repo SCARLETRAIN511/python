33173ef76f773431c9ec3e112e4e94111e17e964
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-10-29 14:00:09 +0800

    Update minimumTotal.py

--- leetcode/minimumTotal.py
+++ leetcode/minimumTotal.py
@@ -6,7 +6,7 @@ class Solution:
         n = len(triangle)
         f = [[0]*n for _ in range(n)]
         f[0][0] = triangle[0][0]
-        
+
         for i in range(1,n):
             f[i][0] = f[i-1][0] + triangle[i][0]
             for j in range(1,i):
@@ -14,3 +14,16 @@ class Solution:
             f[i][i] = f[i-1][i-1] + triangle[i][i]
         
         return min(f[n-1])
+    
+    def minimumTotal2(self,triangle):
+        n = len(triangle)
+        f = [0] * n
+        f[0] = triangle[0][0]
+
+        for i in range(1,n):
+            f[i] = f[i-1] + triangle[i][i]
+            for j in range(i-1,0,-1):
+                f[j] = min(f[j-1],f[j]) + triangle[i][j]
+            f[0] += triangle[i][0]
+        
+        return min(f)
