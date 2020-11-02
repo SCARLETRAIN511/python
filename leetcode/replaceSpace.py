7e88e00324a99a01fe6c736bbc2dcc761cf896d9
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-11-03 00:04:25 +0800

    Update replaceSpace.py

--- leetcode/replaceSpace.py
+++ leetcode/replaceSpace.py
@@ -4,6 +4,9 @@ class Solution:
     def replaceSpace(self,s:str) -> str:
         res = []
         for c in s:
-            if c == '':
+            if c == ' ':
                 res.append('%20')
-                
+            else:
+                res.append(c)
+        return "".join(res)
+
