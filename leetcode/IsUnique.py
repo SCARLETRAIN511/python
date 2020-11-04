3b0da02a64a90652ace48a92ab382adc70ae7322
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-11-04 21:28:15 +0800

    Create IsUnique.py

---
+++ leetcode/IsUnique.py
@@ -0,0 +1,13 @@
+#python3
+
+class Solution:
+    def isUnique(self,astr:str) -> bool:
+        mark = 0
+        for char in astr:
+            moveBit = ord(char) - ord("a")
+            if (mark & (1 << moveBit)) != 0:
+                return False
+            else:
+                mark |= (1<<moveBit)
+        
+        return True
