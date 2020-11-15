fc5496661baff479e8b532b2021225f644fbfa64
SCARLETRAIN511 <41808009+SCARLETRAIN511@users.noreply.github.com>
2020-11-15 23:55:55 +0800

    Update uniquePathsWithoutObstacles.py

--- leetcode/uniquePathsWithoutObstacles.py
+++ leetcode/uniquePathsWithoutObstacles.py
@@ -8,14 +8,14 @@ class Solution:
 
         m,n = len(obstacleGrid),len(obstacleGrid[0])
 
-        dp = [[0]*n]*m
+        dp = [[0]*n for _ in range(m)]
+
         for i in range(0,m):
             if obstacleGrid[i][0] == 1:
                 dp[i][0] = 0
                 break
             else:
                 dp[i][0] = 1
-        print(dp)
 
         for j in range(0,n):
             if obstacleGrid[0][j] == 1:
@@ -24,20 +24,19 @@ class Solution:
             else:
                 dp[0][j] = 1
 
-        print(dp)
+
         for i in range(1,m):
             for j in range(1,n):
-                print(dp)
                 if obstacleGrid[i][j] == 0:
                     dp[i][j] = dp[i-1][j] + dp[i][j-1]
                 else:
                     dp[i][j] = 0
         
-        print(dp)
-
         return dp[m-1][n-1]
+
+
 
 if __name__ == "__main__":
     s = Solution()
-    obstacleGrid = [[0],[1]]
+    obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
     print(s.uniquePathsWithObstacles(obstacleGrid))
